a46db120a6f90dd29dea975fef360c1d7ed75efb
CHESSPOWERINME <97198151+CHESSPOWERINME@users.noreply.github.com>
2022-01-06 17:50:53 +0530

    Update var.py

--- var.py
+++ var.py
@@ -5,20 +5,20 @@ class Var(object):
     APP_ID = int(os.environ.get("APP_ID", 6))
     # 6 is a placeholder
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    LEGEND_STRING = os.environ.get("LEGEND_STRING", None)
+    DARK_STRING = os.environ.get("DARK_STRING", None)
     DB_URI = os.environ.get("DATABASE_URL", None)
     TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", None)
     LOGGER = True
     GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
     GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
     # Here for later purposes
-    SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "2082798662").split())
+    SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", " ").split())
     WHITELIST_USERS = set(int(x) for x in os.environ.get("WHITELIST_USERS", "").split())
     BLACKLIST_USERS = set(int(x) for x in os.environ.get("BLACKLIST_USERS", "").split())
     DEVLOPERS = set(int(x) for x in os.environ.get("DEVLOPERS", "").split())
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())
     SUPPORT_USERS = set(int(x) for x in os.environ.get("SUPPORT_USERS", "").split())
-    PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", "-79269473936"))
+    PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", "PUT UR"))
     LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
